5068f6f9d3c316b05e3260080bc64f47e4e0ea68
filzasaleem <filza.saleem05@gmail.com>
2023-10-25 21:54:04 +0200

    added the start over loop

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -9,16 +9,26 @@ import { HolidayActivities } from "./HolidayActivities";
 import { ExcitmentSlider } from "./ExcitmentSlider";
 
 export const Form = () => {
-  const [formData, setFormData] = useState({
+  const initialFormData = {
     name: "",
     holidayType: "",
     destination: "",
     holidayActivity: "",
     excitmentLevel: 1,
-  });
+  };
+
+  const [formData, setFormData] = useState(initialFormData);
+
   const [currentStep, setCurrentStep] = useState(0);
   const [showSummary, setShowSummary] = useState(false);
   const [buttonState, setButtonState] = useState("let's plan");
+
+  const resetForm = () => {
+    setFormData(initialFormData);
+    setCurrentStep(0);
+    setShowSummary(false);
+    setButtonState("Let's plan");
+  };
 
   const updateFormData = (key, value) => {
     setFormData((values) => ({ ...values, [key]: value }));
@@ -26,10 +36,13 @@ export const Form = () => {
 
   const nextStep = () => {
     if (currentStep < 5) setCurrentStep(currentStep + 1);
-    currentStep > 0 ? setButtonState("Continue") : setButtonState("let's plan");
-
+    // if (currentStep === 0 ) setButtonState("let's plan")
+    // else 
+    // setButtonState("Continue")
   };
 
+  
+  
   // Function to move to the previous step in the form
   const prevStep = () => {
     if (currentStep > 1) setCurrentStep(currentStep - 1);
@@ -45,58 +58,76 @@ export const Form = () => {
     <div>
       {!showSummary ? (
         <form onSubmit={formSubmit}>
-           
-         {currentStep == 0 && 
-             
+          {currentStep == 0 && (
+            <div>
               <HolidayPlanner />
-              
-              
-          }
+            </div>
+          )}
           {currentStep == 1 && (
-            <Name value={formData.name} updateFormData={updateFormData} />
+            <div>
+              <Name value={formData.name} updateFormData={updateFormData} />
+            </div>
           )}
           {currentStep === 2 && (
-            <HolidayType
-              value={formData.holidayType}
-              updateFormData={updateFormData}
-            />
+            <div>
+              <HolidayType
+                value={formData.holidayType}
+                updateFormData={updateFormData}
+              />
+            </div>
           )}
           {currentStep === 3 && (
-            <HolidayActivities
-              value={formData.holidayActivity}
-              updateFormData={updateFormData}
-             holidayType = {formData.holidayType}
-
-            />
+            <div>
+              <HolidayActivities
+                value={formData.holidayActivity}
+                updateFormData={updateFormData}
+                holidayType={formData.holidayType}
+              />
+            </div>
           )}
           {currentStep == 4 && (
-            <HolidayDestination
-              value={formData.destinaton}
-              updateFormData={updateFormData}
-            />
+            <div>
+              <HolidayDestination
+                value={formData.destinaton}
+                updateFormData={updateFormData}
+              />
+            </div>
           )}
           {currentStep == 5 && (
-            <ExcitmentSlider
-              value={formData.excitmentLevel}
-              updateFormData={updateFormData}
-            />
+            <div>
+              <ExcitmentSlider
+                value={formData.excitmentLevel}
+                updateFormData={updateFormData}
+              />
+            </div>
           )}
-          {currentStep > 1 && <button onClick={prevStep}>Back</button>}
-          {currentStep < 5 ? (
-            <button onClick={nextStep}>{buttonState}</button>
-          ) : (
-            <button onClick={renderSummary} type="submit">
-              Submit
-            </button>
-          )}
+          <div className="buttons">
+            {currentStep > 1 && <button onClick={prevStep}>Back</button>}
+            {currentStep < 5 ? (
+              <button onClick={nextStep}> {currentStep === 0 ? "Let's plan" :"Continue"  }</button>
+            ) : (
+              <button onClick={renderSummary} type="submit">
+                Submit
+              </button>
+            )}
+          </div>
+          <div className="progressBar">
+            {currentStep > 0 && (
+              <input type="range" min="1" max="5" value={currentStep}></input>
+            )}
+          </div>
         </form>
       ) : (
-        <FormSummary  
-          name={formData.name}
-          destination={formData.destination}
-          holidayType={formData.holidayType}
-          holidayActivity =  {formData.holidayActivity}
-        />
+        <div>
+          <FormSummary
+            name={formData.name}
+            destination={formData.destination}
+            holidayType={formData.holidayType}
+            holidayActivity={formData.holidayActivity}
+            excitmentLevel={formData.excitmentLevel}
+          />
+          <button onClick={resetForm}>Plan a new trip</button>
+        </div>
       )}
     </div>
   );
--- src/components/FormSummary.jsx
+++ src/components/FormSummary.jsx
@@ -3,15 +3,30 @@ export const FormSummary = ({
   destination,
   holidayType,
   holidayActivity,
+  excitmentLevel,
 }) => {
+  let feelings = "";
+  if (excitmentLevel < 35) {
+    feelings =
+      "Although you might not be super thrilled about the trip, you're still going to have a great time!";
+  } else if (excitmentLevel < 65 && excitmentLevel > 34) {
+    feelings =
+      "You seem to have moderate excitement for the trip, so get ready for some fun and relaxation!";
+  } else {
+    feelings =
+      "With your high level of excitement, you're in for an amazing trip filled with joy and adventure!";
+  }
+
   return (
     <div>
       <h3>Dear {name}</h3>
       <p>
         {" "}
-        We just booked your dream trip to {destination} filled with
-        {" "}{holidayType} activities. Among other things, you are gonna enjoy {" "}
-        {holidayActivity}. Enjoy!
+        We just booked your dream trip to {destination} filled with{" "}
+        {holidayType} type activities. Among other things, you are gonna enjoy{" "}
+        {holidayActivity}. <br />
+        {feelings}
+        Enjoy!
       </p>
     </div>
   );
